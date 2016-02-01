4e18e7f33a2133db12655f28f219975916330291
Rakha Kanz Kautsar <rkkautsar@gmail.com>
2016-02-01 20:35:52 +0700

    Initial commit

---
+++ gulpfile.js
@@ -0,0 +1,77 @@
+var gulp    = require('gulp'),
+	sass    = require('gulp-sass'),
+	nano    = require('gulp-cssnano'),
+	twig    = require('gulp-twig'),
+	bower   = require('gulp-bower'),
+	prefix  = require('gulp-autoprefixer'),
+	jshint  = require('gulp-jshint'),
+	uglify  = require('gulp-uglify'),
+	concat  = require('gulp-concat'),
+	notify  = require('gulp-notify'),
+	rename  = require('gulp-rename'),
+	connect = require('gulp-connect'),
+	del     = require('del');
+
+gulp.task('styles', function(){
+	gulp.src('./src/sass/**/*.scss')
+		.pipe(prefix('last 2 version'))
+		.pipe(sass().on('error', function(e){
+			notify({ message : e });
+		}))
+		.pipe(gulp.dest('./dist/css/'))
+		.pipe(rename({ suffix: '.min' }))
+		.pipe(nano({ discardComments: {removeAll: true} }))
+		.pipe(gulp.dest('./dist/css/'))
+		.pipe(notify('Styles build completed.'))
+		.pipe(connect.reload())
+});
+
+gulp.task('scripts', function(){
+	gulp.src('./src/js/**/*.js') //ignore vendor/
+		.pipe(jshint())
+		.pipe(jshint.reporter('default'))
+		.pipe(concat('script.js'))
+		.pipe(gulp.dest('./dist/js'))
+		.pipe(rename({ 
+			suffix : '.min' 
+		}))
+		.pipe(uglify())
+		.pipe(gulp.dest('js'))
+		.pipe(notify({ message : 'Scripts build completed' }))
+		.pipe(connect.reload())
+});
+
+gulp.task('render', function(){
+	gulp.src(['./src/views/**/*.twig', '!./src/views/includes/**/*.twig'])
+		.pipe(twig({
+			onError : function(e){
+				notify({ message : e });
+			}
+		}))
+		.pipe(gulp.dest('./dist'))
+		.pipe(notify({ message : 'Templates render completed' }))
+		.pipe(connect.reload())
+});
+
+gulp.task('serve', function(){
+	connect.server({
+		root: 'dist',
+		livereload: true
+	})
+});
+
+gulp.task('clean', function(){
+	return del([
+		'./dist/**/*'
+	])
+});
+
+gulp.task('watch', function(){
+	gulp.watch('./src/sass/**/*.scss', ['styles']);
+	gulp.watch('./src/js/**/*.js', ['scripts']);
+	gulp.watch('./src/views/**/*.twig', ['render']);
+});
+
+gulp.task('build', ['clean', 'styles', 'scripts', 'render']);
+
+gulp.task('default', ['build', 'serve', 'watch']);
