79788cae6565fd604eb292d7abe8ee90ceed1050
Rakha Kanz Kautsar <rkkautsar@gmail.com>
2016-02-03 15:21:44 +0700

    Modifications and completed first section - Use gulp-plugins-load - Use gulp-jimp to handle image assets - Add mixins - Remove gutter from grid system

--- gulpfile.js
+++ gulpfile.js
@@ -1,58 +1,61 @@
-var gulp     = require('gulp'),
-	sass     = require('gulp-sass'),
-	nano     = require('gulp-cssnano'),
-	twig     = require('gulp-twig'),
-	bower    = require('gulp-bower'),
-	prefix   = require('gulp-autoprefixer'),
-	jshint   = require('gulp-jshint'),
-	uglify   = require('gulp-uglify'),
-	concat   = require('gulp-concat'),
-	notify   = require('gulp-notify'),
-	rename   = require('gulp-rename'),
-	connect  = require('gulp-connect'),
-	beautify = require('gulp-html-beautify'),
-	del      = require('del');
+var gulp = require('gulp'),
+	$    = require('gulp-load-plugins')(),
+	del  = require('del');
 
 gulp.task('styles', function(){
 	gulp.src('./src/sass/**/*.scss')
-		.pipe(sass().on('error', sass.logError))
-		.pipe(prefix({browsers:'> 5%'}))
+		.pipe($.sass().on('error', $.sass.logError))
+		.pipe($.autoprefixer({browsers:'> 5%'}))
 		.pipe(gulp.dest('./dist/css/'))
-		.pipe(rename({ suffix: '.min' }))
-		.pipe(nano({ discardComments: {removeAll: true} }))
+		.pipe($.rename({ suffix: '.min' }))
+		.pipe($.cssnano({ discardComments: {removeAll: true} }))
 		.pipe(gulp.dest('./dist/css/'))
-		.pipe(notify('Styles build completed.'))
-		.pipe(connect.reload())
+		.pipe($.notify('Styles build completed.'))
+		.pipe($.connect.reload())
 });
 
 gulp.task('scripts', function(){
 	gulp.src('./src/js/**/*.js') //ignore vendor/
-		.pipe(jshint())
-		.pipe(jshint.reporter('default'))
-		.pipe(concat('script.js'))
+		.pipe($.jshint())
+		.pipe($.jshint.reporter('default'))
+		.pipe($.concat('script.js'))
 		.pipe(gulp.dest('./dist/js'))
-		.pipe(rename({ 
+		.pipe($.rename({ 
 			suffix : '.min' 
 		}))
-		.pipe(uglify())
+		.pipe($.uglify())
 		.pipe(gulp.dest('js'))
-		.pipe(notify({ message : 'Scripts build completed' }))
-		.pipe(connect.reload())
+		.pipe($.notify({ message : 'Scripts build completed' }))
+		.pipe($.connect.reload())
 });
 
 gulp.task('render', function(){
 	gulp.src(['./src/views/**/*.twig', '!./src/views/includes/**/*.twig'])
-		.pipe(twig({
+		.pipe($.twig({
 			errorLogToConsole: true
 		}))
-		.pipe(beautify())
+		.pipe($.htmlBeautify())
 		.pipe(gulp.dest('./dist'))
-		.pipe(notify({ message : 'Templates render completed' }))
-		.pipe(connect.reload())
+		.pipe($.notify({ message : 'Templates render completed' }))
+		.pipe($.connect.reload())
+});
+
+gulp.task('images', function(){
+	gulp.src(['./src/assets/*-background.{png,jpg,jpeg}'])
+		.pipe($.jimp({
+			'' : {
+				resize: {width: 1280},
+				brightness: -0.5,
+				blur: 10,
+				quality: 70
+			}
+		}))
+		.pipe(gulp.dest('./dist/img'))
+		.pipe($.notify({ message : 'Image tasks completed' }))
 });
 
 gulp.task('serve', function(){
-	connect.server({
+	$.connect.server({
 		root: 'dist',
 		livereload: true
 	})
@@ -70,7 +73,7 @@ gulp.task('watch', function(){
 	gulp.watch('./src/views/**/*.twig', ['render']);
 });
 
-gulp.task('build', ['styles', 'scripts', 'render']);
+gulp.task('build', ['styles', 'scripts', 'images', 'render']);
 
 gulp.task('rebuild', ['clean', 'build']);
 
